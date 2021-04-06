67a476502cf999fedeba6127b99c012190ac5316
unknown <joemukaratirwa@gmail.com>
2021-04-07 00:10:34 +0200

    added a new line of code for instructions and also corrected the terminology

--- quadratic3.c
+++ quadratic3.c
@@ -6,22 +6,23 @@ int main()
     double a;
     double b;
     double c;
-    printf("This is a quadratic equations' calculator\n");
-    printf("Please enter the first value for a --> ");
+    printf("Quadratic equations' calculator\n");
+    printf("To calculate the roots enter the coefficients accordingly");
+    printf("Please enter the first coefficient --> ");
     scanf("%lf",&a);
-    printf("Please enter the second value for b --> ");
+    printf("Please enter the second coefficient  --> ");
     scanf("%lf",&b);
-    printf("Please enter the second value for c --> ");
+    printf("Please enter the second coefficient --> ");
     scanf("%lf",&c);
     
-    printf("You have the quadratic expression: %fx^2 + %fx + %f\n",a ,b ,c);
+    printf("You have the following quadratic expression: %fx^2 + %fx + %f\n",a ,b ,c);
     double delta = ( b * b ) - ( 4 * a * c);
     double root1 = (-b + sqrt( delta)) / ( 2 * a );
     double root2 = (-b - sqrt(delta)) / ( 2 * a );
 
     if( delta == 0)
     { 
-      printf("root1 is equal to root2\n");
+      printf("root 1 is equal to root 2\n");
       printf("The root is: %f\n",root1);
     }
     if( delta > 0 )
